27a656241bf771492856f1c131f0660d43016892
Adil Waqar <adwiq78@gmail.com>
2019-02-04 18:44:11 +0500

    rendered

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const router = express.Router();
+const bodyParser = require('body-parser');
+let app = express();
+
+// Load views and view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Body parser middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Setting public folder for statis assets
 app.use(express.static(path.join(__dirname, 'vendor')));
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
-
-console.log('Running at Port 3000');
+app.listen(4000, (err) => {
+  console.log('Server started on port 4000');
+});
